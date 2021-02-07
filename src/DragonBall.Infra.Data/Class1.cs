6b46d77a2e2e2bfbded387844496ad9b236abbeb
magomes <m.gomescontato@gmail.com>
2021-02-06 21:38:38 -0300

    teste feature branches

---
+++ src/DragonBall.Infra.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonBall.Infra.Data.Repositories
+{
+    class Class1
+    {
+    }
+}
